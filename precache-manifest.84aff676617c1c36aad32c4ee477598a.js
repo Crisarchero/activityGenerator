b3d95003a8aa2dbb76deb8959247acae9022b73c
Crisarchero <YOUNGERBROWN123@GMAIL.COM>
2023-02-14 11:04:03 -0600

    Updates

---
+++ precache-manifest.84aff676617c1c36aad32c4ee477598a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ec5d8b2e57002b1384c",
+    "url": "/activityGenerator/static/css/main.1e5793b4.chunk.css"
+  },
+  {
+    "revision": "7ec5d8b2e57002b1384c",
+    "url": "/activityGenerator/static/js/main.7ec5d8b2.chunk.js"
+  },
+  {
+    "revision": "4d80f5d4461cadf88fea",
+    "url": "/activityGenerator/static/js/1.4d80f5d4.chunk.js"
+  },
+  {
+    "revision": "e5848f2a8e2085e61a38",
+    "url": "/activityGenerator/static/js/runtime~main.e5848f2a.js"
+  },
+  {
+    "revision": "afbfbe44dce379b350b450f6d35615f7",
+    "url": "/activityGenerator/static/media/waves.afbfbe44.mp4"
+  },
+  {
+    "revision": "41bc5131d2f98432f6e8fae0f918c734",
+    "url": "/activityGenerator/index.html"
+  }
+];
